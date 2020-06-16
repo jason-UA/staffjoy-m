623ff93a6abb474dc0d2681fe4baaeaa91774126
jason <Jason.Wang2@harman.com>
2020-06-16 16:44:11 +0800

    auth svc

---
+++ auth-svc/src/main/java/com/example/auth/configure/AuthWebSecurityConfigure.java
@@ -0,0 +1,47 @@
+package com.example.auth.configure;
+
+import com.example.auth.service.AuthUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class AuthWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private AuthUserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/oauth/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**")
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+}
---
+++ auth-svc/src/main/java/com/example/auth/dto/AuthUser.java
@@ -0,0 +1,20 @@
+package com.example.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthUser {
+    private static final long serialVersionUID = -1748289340320186418L;
+
+    private String username;
+
+    private String password;
+
+    private boolean accountNonExpired = true;
+
+    private boolean accountNonLocked= true;
+
+    private boolean credentialsNonExpired= true;
+
+    private boolean enabled= true;
+}
