baa67b560c06441be1d56d475d476df4722ca941
jason <Jason.Wang2@harman.com>
2020-06-19 17:32:54 +0800

    account remove security

--- account-svc/src/main/java/com/example/account/AccountApplication.java
+++ account-svc/src/main/java/com/example/account/AccountApplication.java
@@ -3,7 +3,6 @@ package com.example.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 public class AccountApplication {
